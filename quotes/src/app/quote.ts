cd542d5447d8806420600c93de3e143c1abbb490
Chris-karanja <chriswainaina0@gmail.com>
2018-10-19 11:15:44 +0300

    added a boolean to toggle quote details

--- quotes/src/app/quote.ts
+++ quotes/src/app/quote.ts
@@ -1,3 +1,6 @@
 export class Quote {
-    constructor(public quoteID:number, public message:string, public author:string, public upVote:number, public downVote:number, public dateAdded:Date){}
+    public showDetail:boolean;
+    constructor(public quoteID:number, public message:string, public author:string, public upVote:number, public downVote:number, public dateAdded:Date){
+        this.showDetail=false
+    }
 }
