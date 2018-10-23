7c684faa886d137bf1f7904d3be716b5aecf04ae
Chris-karanja <chriswainaina0@gmail.com>
2018-10-23 18:11:16 +0300

    added bootstrap elements and quotes

--- quotes/src/app/quote/quote.component.ts
+++ quotes/src/app/quote/quote.component.ts
@@ -8,11 +8,11 @@ import { from } from 'rxjs';
 })
 export class QuoteComponent implements OnInit {
   userQuotes = [
-    new Quote(1, 'Quote number 1', 'oriso', 0, 0, new Date(2018,7,10)),
-    new Quote(2, 'Quote number 2', 'oriso', 0, 0, new Date(2018,7,10)),
-    new Quote(3, 'Quote number 3', 'oriso', 0, 0, new Date(2018,7,10)),
-    new Quote(4, 'Quote number 4', 'oriso', 0, 0, new Date(2018,7,10)),
-    new Quote(5, 'Quote number 5', 'oriso', 0, 0, new Date(2018,7,10))
+    new Quote(1, 'I just knew there were stories I wanted to tell.', 'Octavia E. Butler', 0, 0, new Date(2018,23,10)),
+    new Quote(2, 'All good books are alike in that they are truer than if they really happened and after you are finished reading one you feel that it all happened to you and after which it all belongs to you.', '- Ernest Hemingway', 0, 0, new Date(2018,23,10)),
+    new Quote(3, 'When written in Chinese, the word crisis is composed of two characters. One represents danger and the other represents opportunity.', '- John F. Kennedy', 0, 0, new Date(2018,23,10)),
+    new Quote(4, 'The play was a great success, but the audience was a disaster.', 'Oscar Wilde', 0, 0, new Date(2018,23,10)),
+    new Quote(5, 'Noise proves nothing. Often a hen who has merely laid an egg cackles as if she had laid an asteroid.', ' Mark Twain', 0, 0, new Date(2018,7,10))
   ]
   toogleDetails(index){
     this.userQuotes[index].showDetail = !this.userQuotes[index].showDetail
